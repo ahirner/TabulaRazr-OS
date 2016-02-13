4718561269a60602d94af294342ac24a7f161dd0
David Krmpotic <david.krmpotic@gmail.com>
2016-02-13 18:37:36 +0100

    prevent the issue when /ug folder doesn't exist on the first run

--- server.py
+++ server.py
@@ -11,7 +11,7 @@
 #1) each row is either in one line or not a row at all
 #2) each column features at least one number (=dollar amount)
 #2a) each column features at least one date-like string [for time-series only]
-#3) a table exists if rows are in narrow consecutive order and share similarities --> scoring algo [DONE] 
+#3) a table exists if rows are in narrow consecutive order and share similarities --> scoring algo [DONE]
 #4) each column is separated by more than x consecutive whitespace indicators (e.g. '  ' or '..')
 
 #Feature List Todo:
@@ -20,7 +20,7 @@
 #3) improve captioning: expand non canonical values in tables [DONE] .. but not to the extent how types match up  --> use this to further
 ## delineate between caption and headers
 #4) UI: parameterize extraction on the show page on the fly
-#5) deeper type inference on token level: type complex [DONE], subtype header (centered, capitalized), 
+#5) deeper type inference on token level: type complex [DONE], subtype header (centered, capitalized),
 ## subtype page nr., type free flow [DONE, need paragraph]
 #5a) re
 #6) Respect negative values with potential '-' for numerical values
@@ -94,10 +94,10 @@ def tag_token(token, ws):
             leftover = token[:result.start()], token[result.end():]
             lr = "".join(leftover)
             value = token[result.start():result.end()]
-            
+
             if len(lr) >= config['complex_leftover_threshold']:
                 return "complex", "unknown", token, leftover
-            
+
             subtype = "none"
             #First match on left-overs
             for sub, indicator in subtype_indicator.iteritems():
@@ -111,10 +111,10 @@ def tag_token(token, ws):
                 for sub, indicator in subtype_indicator.iteritems():
                     if re.match(indicator, ws): subtype = sub
             #print token, ":", ws, ":", subtype
-                        
+
             return t, subtype, value, leftover
     return "unknown", "none", token, ""
-    
+
 def row_feature(line):
     matches = re.finditer(tokenize_pattern, line)
     start_end = [ (match.start(), match.end()) for match in matches]
@@ -124,13 +124,13 @@ def row_feature(line):
             return []
         else:
             return [{'start' : 0, 'value' : line, 'type' : 'freeform', 'subtype' : 'none'}]
-    
+
     tokens = re.split(tokenize_pattern, line)
-    if tokens[0] == "": 
+    if tokens[0] == "":
         tokens = tokens[1:]
     else:
         start_end = [(0,0)] + start_end
-    
+
     features = []
     for se, token in zip(start_end, tokens):
         t, subtype, value, leftover = tag_token(token, line[se[0]:se[1]])
@@ -155,17 +155,17 @@ def row_equal_types(row1, row2):
 # In[7]:
 
 #Non qualified rows arm for consistency check but are tolerated for max_grace_rows (whitespace, breakline, junk)
-def filter_row_spans_new(row_features, row_qualifies=row_qualifies, ):    
+def filter_row_spans_new(row_features, row_qualifies=row_qualifies, ):
 
     min_consecutive = config["min_consecutive_rows"]
     grace_rows = config['max_grace_rows']
 
-    last_qualified = None    
+    last_qualified = None
     consecutive = 0
     underqualified = 0
     consistency_check = False
     i = 0
-    
+
     for j, row in enumerate(row_features):
         qualifies = row_qualifies(row)
         if consistency_check:
@@ -174,13 +174,13 @@ def filter_row_spans_new(row_features, row_qualifies=row_qualifies, ):
                 qualifies = False
             consistency_check = False
         #print qualifies, row_to_string(row)
-        
+
         if qualifies:
             if last_qualified is None:
                 last_qualified = i
                 consecutive = 1
             else:
-                consecutive += 1    
+                consecutive += 1
         else:
             underqualified += 1
             if underqualified > grace_rows:
@@ -189,19 +189,19 @@ def filter_row_spans_new(row_features, row_qualifies=row_qualifies, ):
                     print "YIELDED from", last_qualified, "to", i-underqualified+1
                     yield last_qualified, i-underqualified+1
 
-                last_qualified = None                
+                last_qualified = None
                 consecutive = 0
                 underqualified = 0
                 consistency_check = False
             else:
-                if last_qualified: 
+                if last_qualified:
                     consistency_check = True
         print i, last_qualified, consecutive, consistency_check, row_to_string(row)
         i += 1
-        
+
     if consecutive >= min_consecutive:
         yield last_qualified, i-underqualified
-        
+
 def row_to_string(row, key='value', sep='|'):
     return sep.join(c[key] for c in row)
 
@@ -215,21 +215,21 @@ def row_type_compatible(row_canonical, row_test):
             no_fit += 1
 
     fraction_no_fit = no_fit / float(len(row_test))
-    #print "test row", row_to_string(row_test), ") against types (", row_to_string(row_canonical, 'type'), ") has %f unmatching types" % fraction_no_fit    
+    #print "test row", row_to_string(row_test), ") against types (", row_to_string(row_canonical, 'type'), ") has %f unmatching types" % fraction_no_fit
     return fraction_no_fit < config["threshold_caption_extension"]
 
-def filter_row_spans(row_features, row_qualifies):    
+def filter_row_spans(row_features, row_qualifies):
 
     min_consecutive = config["min_consecutive_rows"]
     grace_rows = config['max_grace_rows']
 
-    last_qualified = None    
+    last_qualified = None
     consecutive = 0
     underqualified = 0
-    underqualified_rows = [] #Tuples of row number and the row    
-    
+    underqualified_rows = [] #Tuples of row number and the row
+
     i = 0
-    
+
     for j, row in enumerate(row_features):
         if row_qualifies(row):
             underqualified = 0
@@ -237,7 +237,7 @@ def filter_row_spans(row_features, row_qualifies):
                 last_qualified = i
                 consecutive = 1
             else:
-                consecutive += 1    
+                consecutive += 1
         else:
             underqualified += 1
             underqualified_rows.append((j, row) )
@@ -250,7 +250,7 @@ def filter_row_spans(row_features, row_qualifies):
                 underqualified = 0
         print i, underqualified, last_qualified, consecutive#, "" or row
         i += 1
-        
+
     if consecutive >= min_consecutive:
         yield last_qualified, i-underqualified
 
@@ -277,12 +277,12 @@ def readjust_cols(feature_row, slots):
 
 
 def normalize_rows(rows_in, structure):
-    slots = [c['start'] for c in structure] 
+    slots = [c['start'] for c in structure]
     nrcols = len(structure)
-    
+
     for r in rows_in:
         if len(r) != nrcols:
-            if len(r)/float(nrcols) > config['threshold_caption_extension']:          
+            if len(r)/float(nrcols) > config['threshold_caption_extension']:
                 yield readjust_cols(r, slots)
         else:
             yield r
@@ -297,19 +297,19 @@ def structure_rows(row_features, meta_features):
         if nr_of_l_rows/float(len(row_features)) > config['min_canonical_rows']:
             nrcols = l
             break
-            
+
     canonical = filter(lambda r: len(r) == nrcols , row_features)
-    
+
     #for c in canonical: print len(c), row_to_string(c)
-        
+
     structure = []
     for i in range(nrcols):
         col = {}
         col['start'] = float (sum (c[i]['start'] for c in canonical )) / len(canonical)
-    
+
         types = Counter(c[i]['type'] for c in canonical)
         col['type'] = types.most_common(1)[0][0]
-        subtypes = Counter(c[i]['subtype'] for c in canonical if c[i]['subtype'] is not "none")        
+        subtypes = Counter(c[i]['subtype'] for c in canonical if c[i]['subtype'] is not "none")
         subtype = "none" if len(subtypes) == 0 else subtypes.most_common(1)[0][0]
         col['subtype'] = subtype
         structure.append(col)
@@ -322,14 +322,14 @@ def structure_rows(row_features, meta_features):
         else:
             meta_features.append(r)
             row_features.remove(r)
-     
+
     meta_features.reverse()
     #for m in meta_features: print "META", row_to_string(m)
- 
+
     captions = [''] * nrcols
     single_headers = []
     latest_caption_len = 1
-    slots = [c['start'] for c in structure] 
+    slots = [c['start'] for c in structure]
     for mf in meta_features:
         #if we have at least two tokens in the line, consider them forming captions
         nr_meta_tokens = len(mf)
@@ -338,22 +338,22 @@ def structure_rows(row_features, meta_features):
             for c in mf:
                 dist = (abs((float(c['start'])) - s) for s in slots)
                 val, idx = min((val, idx) for (idx, val) in enumerate(dist))
-                if val <= config['caption_assign_tolerance']: 
+                if val <= config['caption_assign_tolerance']:
                     captions[idx] = c['value'] + ' ' + captions[idx]
                 else: single_headers.append(c['value'])
             #latest_caption_len = nr_meta_tokens
-        #otherwise, throw them into headers directly for now                                                                           
+        #otherwise, throw them into headers directly for now
         else:
             #Only use single tokens to become headers, throw others away
             if len(mf) == 1 and mf[0]['type'] != 'freeform': single_headers.append(mf[0]['value'])
-    
+
 
     #Assign captions as the value in structure
     for i, c in enumerate(captions):
         structure[i]['value'] = c
     #Expand all the non canonical rows with NaN values (Todo: if types are very similar)
-    normalized_data = [r for r in normalize_rows(row_features, structure)]            
-    
+    normalized_data = [r for r in normalize_rows(row_features, structure)]
+
     return structure, normalized_data, single_headers
 
 
@@ -367,13 +367,13 @@ def convert_to_table(rows, b, e, above):
 
     captions = [(col['value'] if 'value' in col.keys() else "---") +"\n(%s, %s)" % (col['type'], col['subtype']) for col in structure]
     table['captions'] = captions
-    table['data'] = data           
+    table['data'] = data
     table['header'] = " | ".join(headers)
 
-    return table 
+    return table
 
 def indexed_tables_from_rows(row_features):
-    
+
     #Uniquely identify tables by their first row
     tables = OrderedDict()
     last_end = 0
@@ -382,17 +382,17 @@ def indexed_tables_from_rows(row_features):
         #Todo: manage the lower meta lines
         tables[b] = convert_to_table(row_features, b, e, min(config['meta_info_lines_above'], b - last_end))
         last_end = tables[b]['end_line']
-    return tables    
-    
+    return tables
+
 def return_tables(txt_path):
-    
+
     #Uniquely identify tables by their first row
     tables = OrderedDict()
-    
+
     with codecs.open(txt_path, "r", "utf-8") as f:
         lines = [l.replace(u'\n', '').replace(u'\r', '') for l in f]
-        rows = [row_feature(l) for l in lines] 
-        
+        rows = [row_feature(l) for l in lines]
+
         return indexed_tables_from_rows(rows)
 
 def table_to_df(table):
@@ -430,7 +430,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def get_extension(filename):
-    return '.' in filename and            filename.rsplit('.', 1)[1] 
+    return '.' in filename and            filename.rsplit('.', 1)[1]
 
 def allowed_file(filename):
     return get_extension(filename) in ALLOWED_EXTENSIONS
@@ -443,7 +443,10 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             extension = get_extension(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], extension, filename))
+            path = os.path.join(app.config['UPLOAD_FOLDER'], extension)
+            if not os.path.exists(path):
+              os.makedirs(path)
+            file.save(os.path.join(path, filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename, min_columns=min_columns))
 
@@ -454,11 +457,21 @@ def upload_file():
 @app.route('/show/<filename>')
 def uploaded_file(filename):
     extension = get_extension(filename)
-    path = os.path.join(app.config['UPLOAD_FOLDER'], extension, filename)
-    txt_path = os.path.join(app.config['UPLOAD_FOLDER'], 'txt', filename)
+
+    pdf_folder = os.path.join(app.config['UPLOAD_FOLDER'], extension)
+    txt_folder = os.path.join(app.config['UPLOAD_FOLDER'], 'txt')
+
+    path = os.path.join(pdf_folder, filename)
+    txt_path = os.path.join(txt_folder, filename)
+
+    if not os.path.exists(pdf_folder):
+      os.makedirs(pdf_folder)
+    if not os.path.exists(txt_folder):
+      os.makedirs(txt_folder)
+
     if extension == "pdf":
         txt_path += '.txt'
-        filename += '.txt'        
+        filename += '.txt'
         if not os.path.isfile(txt_path):
             #Layout preservation crucial to preserve clues about tabular data
             cmd = "pdftotext -layout %s %s" % (path, txt_path)
@@ -481,7 +494,7 @@ def uploaded_file(filename):
     dr = pd.DataFrame()
     dr['value'] = nr_data_rows
     dr['page'] = range(0, len(dr))
-    
+
     #plot the row density
     chart = filename+".png"
     fig, ax = plt.subplots( nrows=1, ncols=1, figsize=(8,3) )  # create figure & 1 axis
@@ -493,7 +506,7 @@ def uploaded_file(filename):
     plt.close(fig)                      # close the figure
 
     #Create HTML
-    notices = ['Extraction Results for ' + filename, 'Ordered by lines']    
+    notices = ['Extraction Results for ' + filename, 'Ordered by lines']
     dfs = (table_to_df(table).to_html() for table in tables.values())
     headers = []
     for t in tables.values():
@@ -516,20 +529,20 @@ def inspector(filename):
     end_line = int(request.args.get('data_end'))
     margin_top = config["meta_info_lines_above"]
     margin_bottom = margin_top
-    
+
     notices = ['showing data lines from %i to %i with %i meta-lines above and below' % (begin_line, end_line, margin_top)]
     with codecs.open(path, "r", "utf-8") as file:
         lines = [l.encode('utf-8') for l in file][begin_line - margin_top:end_line + margin_bottom]
         top_lines = lines[:margin_top]
         table_lines = lines[margin_top:margin_top+end_line-begin_line]
         bottom_lines = lines[margin_top+end_line-begin_line:]
-    
+
     offset = begin_line-margin_top
     table_id = begin_line
-    
+
     return render_template('inspector.html',
         title=TITLE,
-        base_scripts=scripts, css=css, notices = notices, filename=filename, top_lines=top_lines, 
+        base_scripts=scripts, css=css, notices = notices, filename=filename, top_lines=top_lines,
         table_lines=table_lines, bottom_lines=bottom_lines, offset=offset, table_id=begin_line)
 
 
@@ -549,7 +562,7 @@ else:
     app.run(debug=True, host='0.0.0.0', port = PORT)
 
 
-# 
+#
 # ## Tests ##
 
 # In[ ]:
@@ -660,7 +673,7 @@ FY 2013-14, revenues increased at a rate of 10.8 percent and expenses increased 
 from IPython.display import display
 
 #print i, underqualified, last_qualified, consecutive#, "" or row
-        
+
 rows = [row_feature(l) for l in test_string]
 
 for b, e in filter_row_spans(rows, row_qualifies):
@@ -669,16 +682,16 @@ for b, e in filter_row_spans(rows, row_qualifies):
     #    print i, len(rows[i]), row_to_string(rows[i])
 
 print "#######################"
-    
+
 tables = indexed_tables_from_rows(rows)
 for begin_line, t in tables.iteritems():
     df = table_to_df(t)
-    
+
     #for d in t['data']: print row_to_string(d)
-    
+
     for j in range(t['begin_line']-4, t['begin_line']):
         pass
-        
+
     for j in range(t['begin_line'], t['end_line']):
         pass #print len(rows[j]), test_string[j], "|".join([c['type']+'_'+c['subtype'] for c in rows[j]])
     print t['header']
@@ -754,7 +767,7 @@ test_string ="""
 
      $24,535,000 4.000% Term Bonds due October 1, 2040 – Yield: 3.400%(c); CUSIP†: 13048TUR2
      $13,145,000 5.250% Term Bonds due October 1, 2045 – Yield: 2.970%(c); CUSIP†: 13048TUS0
-          
+
 """.decode('ascii', 'ignore').split("\n")
 
 
@@ -883,7 +896,7 @@ Potential investors must read the entire Official Statement, including the Appen
 
 # In[ ]:
 
-try: 
+try:
     from IPython.display import display
 
     rows = [row_feature(l) for l in test_string]
